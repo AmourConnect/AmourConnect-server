8c3105c2cbba1d3164b1856d1554b79e599eb79a
Lyamss <161554647+lyamss@users.noreply.github.com>
2024-07-07 16:43:27 +0200

    Fix bug OAuth Login Google when User Cancel Auth

--- src/AmourConnect.App/Interfaces/Controllers/IAuthCase.cs
+++ src/AmourConnect.App/Interfaces/Controllers/IAuthCase.cs
@@ -4,7 +4,7 @@ namespace AmourConnect.App.Interfaces.Controllers
 {
     public interface IAuthCase
     {
-        Task<bool> ValidateGoogleLoginAsync();
+        Task<(bool success, string message)> ValidateGoogleLoginAsync();
         Task<(bool success, string message)> RegisterUserAsync(SetUserRegistrationDto setuserRegistrationDto, string userIdGoogle, string emailGoogle);
     }
 }
