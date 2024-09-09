b7fb8235effb9145373da8f8010aed38bea1c547
Lyamss <161554647+lyamss@users.noreply.github.com>
2024-09-10 01:57:02 +0200

    fix bug required EndPoints Patch User

--- src/AmourConnect.Domain/Dtos/SetDtos/SetUserUpdateDto.cs
+++ src/AmourConnect.Domain/Dtos/SetDtos/SetUserUpdateDto.cs
@@ -1,21 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System.ComponentModel.DataAnnotations;
 
 namespace AmourConnect.Domain.Dtos.SetDtos
 {
     public class SetUserUpdateDto
     {
-        [Required]
         public IFormFile? Profile_picture { get; set; }
 
-        [Required]
         public string? city { get; set; }
 
-        [Required]
         public string? Description { get; set; }
-        [Required]
         public string? sex { get; set; }
-        [Required]
         public DateTime? date_of_birth { get; set; }
     }
 }
